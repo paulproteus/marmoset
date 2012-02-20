6f8a7c36d2ec653ad88da6f940c37dfb1be44d05
Bill Pugh <pugh@cs.umd.edu>
2012-02-20 10:15:22 -0500

    Move marmoset-schema out of dbScripts, delete dbScripts

--- SubmitServerModelClasses/dbScripts/cache_names.sql
+++
@@ -1,6 +0,0 @@
-UPDATE
-student_registration, students
-SET
-student_registration.firstname = students.firstname,
-student_registration.lastname = students.lastname
-WHERE student_registration.student_pk = students.student_pk;
