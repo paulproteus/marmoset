8db04ca6c3ce177e0b136f48477e50c54edf6a8b
Ryan W Sims <rwsims@gmail.com>
2012-02-22 17:09:40 -0500

    Fixed some gwt compiler errors.

--- SubmitServer2/junit/edu/umd/review/ViewImplTestSuite.java
+++ SubmitServer2/junit/edu/umd/review/ViewImplTestSuite.java
@@ -1,9 +1,12 @@
-package edu.umd.review.gwt.view.impl;
+package edu.umd.review;
 
 import junit.framework.Test;
 import junit.framework.TestSuite;
 
 import com.google.gwt.junit.tools.GWTTestSuite;
+
+import edu.umd.review.gwt.view.impl.DraftViewImplTest;
+import edu.umd.review.gwt.view.impl.ThreadViewImplTest;
 
 public class ViewImplTestSuite extends GWTTestSuite {
   public static Test suite() {
