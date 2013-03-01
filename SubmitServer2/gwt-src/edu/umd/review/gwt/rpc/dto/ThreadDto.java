6ad00a6b28420a1351249f29af5680c6d74b0994
Bill Pugh <pugh@cs.umd.edu>
2013-03-01 08:14:01 -0500

    fix reference to client.GWT

--- SubmitServer2/gwt-src/edu/umd/review/gwt/rpc/dto/ThreadDto.java
+++ SubmitServer2/gwt-src/edu/umd/review/gwt/rpc/dto/ThreadDto.java
@@ -2,6 +2,8 @@ package edu.umd.review.gwt.rpc.dto;
 
 import java.util.Collection;
 import java.util.TreeSet;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.annotation.CheckForNull;
 import javax.annotation.Nullable;
@@ -12,24 +14,28 @@ import com.google.common.base.Objects;
 import com.google.common.base.Preconditions;
 import com.google.common.collect.ComparisonChain;
 import com.google.common.collect.Sets;
-import com.google.gwt.core.client.GWT;
 
 import edu.umd.cs.marmoset.modelClasses.CodeReviewThread;
 
 /**
- * DTO for a thread. A thread belongs to a single file, and has 0 or more published comments, and 0
- * or 1 draft comments.
- *
+ * DTO for a thread. A thread belongs to a single file, and has 0 or more
+ * published comments, and 0 or 1 draft comments.
+ * 
  * @author rwsims@umd.edu (Ryan W Sims)
  */
 public class ThreadDto implements Result, Comparable<ThreadDto> {
-  private  @CodeReviewThread.PK  int id;
+  private static final Logger logger = Logger.getLogger(ThreadDto.class.getName());
+  
+  private @CodeReviewThread.PK
+  int id;
   private String file;
   private int line;
   private long timestamp;
   private TreeSet<CommentDto> publishedComments = Sets.newTreeSet();
-  @Nullable private CommentDto draft;
-  @Nullable private RubricEvaluationDto rubricEvaluation;
+  @Nullable
+  private CommentDto draft;
+  @Nullable
+  private RubricEvaluationDto rubricEvaluation;
   private boolean needsResponse;
 
   /** @deprecated GWT only. */
@@ -41,14 +47,15 @@ public class ThreadDto implements Result, Comparable<ThreadDto> {
     this.line = 0xdeadbeef;
   }
 
-  public ThreadDto( @CodeReviewThread.PK  int id, String file, int line, boolean needsResponse) {
+  public ThreadDto(@CodeReviewThread.PK int id, String file, int line, boolean needsResponse) {
     this.id = id;
     this.file = file;
     this.line = line;
     this.needsResponse = needsResponse;
   }
 
-  public   @CodeReviewThread.PK  int getId() {
+  public @CodeReviewThread.PK
+  int getId() {
     return id;
   }
 
@@ -85,31 +92,33 @@ public class ThreadDto implements Result, Comparable<ThreadDto> {
   }
 
   /**
-   * Set the draft for this thread. The draft can be null, which indicates that a preexisting draft
-   * has been published. Cannot set a draft on a thread that already has one.
+   * Set the draft for this thread. The draft can be null, which indicates that
+   * a preexisting draft has been published. Cannot set a draft on a thread that
+   * already has one.
    */
   public void setDraft(CommentDto draft) {
     if (draft != null) {
       Preconditions.checkArgument(draft.isDraft(), "Can't assign published comment " + draft.getId() + " as draft");
       if (this.draft != null) {
-          GWT.log("Duplication drafts " + this.draft.getId() + " and " + draft.getId());
-          if (this.draft.getId() > draft.getId())
-              return;
+        logger.warning("Duplication drafts " + this.draft.getId() + " and " + draft.getId());
+        if (this.draft.getId() > draft.getId())
+          return;
       }
-  }
+    }
     this.draft = draft;
   }
 
   /**
-   * Return the active draft for this thread authored by the current user, if there is such a draft.
+   * Return the active draft for this thread authored by the current user, if
+   * there is such a draft.
    */
   public CommentDto getDraft() {
     return this.draft;
   }
 
   /**
-   * Set the rubric for this thread; it is an error to set a rubric for a thread that already has
-   * one.
+   * Set the rubric for this thread; it is an error to set a rubric for a thread
+   * that already has one.
    */
   public void setRubricEvaluation(RubricEvaluationDto rubricEvaluation) {
     if (this.rubricEvaluation != null && rubricEvaluation != null) {
@@ -123,12 +132,14 @@ public class ThreadDto implements Result, Comparable<ThreadDto> {
   }
 
   /**
-   * Indicate whether the thread needs a response (an acknowledge comment) from a given reviewer. A
-   * reviewer may respond to a thread if all of the following hold:
+   * Indicate whether the thread needs a response (an acknowledge comment) from
+   * a given reviewer. A reviewer may respond to a thread if all of the
+   * following hold:
    * <ol>
    * <li>There is at least 1 published comment in the thread.
    * <li>The last published comment in the thread is not an acknowledge comment.
-   * <li>The last published comment in the thread is not authored by the reviewer.
+   * <li>The last published comment in the thread is not authored by the
+   * reviewer.
    * </ol>
    */
   public boolean needsResponse() {
@@ -136,7 +147,7 @@ public class ThreadDto implements Result, Comparable<ThreadDto> {
   }
 
   public boolean isVisibleInTray() {
-      return (this.draft != null || needsResponse());
+    return (this.draft != null || needsResponse());
   }
 
   @Override
@@ -155,8 +166,8 @@ public class ThreadDto implements Result, Comparable<ThreadDto> {
   /** Implements comparison inconsistent with {@code equals()}. */
   @Override
   public int compareTo(ThreadDto that) {
-    return ComparisonChain.start().compare(this.line, that.line)
-        .compare(this.timestamp, that.timestamp).compare(this.id, that.id).result();
+    return ComparisonChain.start().compare(this.line, that.line).compare(this.timestamp, that.timestamp)
+        .compare(this.id, that.id).result();
   }
 
   public boolean isEmpty() {
@@ -165,8 +176,6 @@ public class ThreadDto implements Result, Comparable<ThreadDto> {
 
   @Override
   public String toString() {
-    return Objects.toStringHelper(this)
-        .add("id", getId())
-        .toString();
+    return Objects.toStringHelper(this).add("id", getId()).toString();
   }
 }
