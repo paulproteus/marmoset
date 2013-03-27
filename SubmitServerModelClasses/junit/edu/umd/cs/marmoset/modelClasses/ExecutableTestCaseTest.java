dbec4ec0379eef7eab8d129070613ee05c748be9
Bill Pugh <pugh@cs.umd.edu>
2013-03-26 23:29:08 -0400

    Handle refactoring of TextDiff

--- SubmitServerModelClasses/junit/edu/umd/cs/marmoset/modelClasses/ExecutableTestCaseTest.java
+++ SubmitServerModelClasses/junit/edu/umd/cs/marmoset/modelClasses/ExecutableTestCaseTest.java
@@ -7,7 +7,7 @@ import java.util.Properties;
 
 import junit.framework.TestCase;
 import edu.umd.cs.marmoset.modelClasses.ExecutableTestCase.InputKind;
-import edu.umd.cs.marmoset.modelClasses.ExecutableTestCase.OutputKind;
+import edu.umd.cs.marmoset.modelClasses.ExecutableTestCase.ExpectedKind;
 import edu.umd.cs.marmoset.modelClasses.ExecutableTestCase.Property;
 import edu.umd.cs.marmoset.modelClasses.TestOutcome.TestType;
 
@@ -19,7 +19,7 @@ public class ExecutableTestCaseTest extends TestCase {
         String testProps = "simpleTest.properties";
         Map<String, ExecutableTestCase> cases =  getTestProperties(testProps);
         ExecutableTestCase tt = cases.get("tt");
-        check(tt, TestType.PUBLIC, InputKind.STRING,  "True True", OutputKind.STRING, "True",
+        check(tt, TestType.PUBLIC, InputKind.STRING,  "True True", ExpectedKind.STRING, "True",
                 "python main.py");
     }
 
@@ -37,17 +37,17 @@ public class ExecutableTestCaseTest extends TestCase {
     public void testComplicatedScriptProperties(String testProps) throws IOException {
         Map<String, ExecutableTestCase> cases =  getTestProperties(testProps);
         ExecutableTestCase p1 = cases.get("p1");
-        check(p1, TestType.PUBLIC, InputKind.FILE, "p1.in", OutputKind.FILE, "p1.out",
+        check(p1, TestType.PUBLIC, InputKind.FILE, "p1.in", ExpectedKind.FILE, "p1.out",
                 "python p1.py");
         ExecutableTestCase p2 = cases.get("p2");
-        check(p2, TestType.PUBLIC, InputKind.FILE, "p2.in", OutputKind.FILE, "p2.out",
+        check(p2, TestType.PUBLIC, InputKind.FILE, "p2.in", ExpectedKind.FILE, "p2.out",
                 "python p2.py");
         ExecutableTestCase r1 = cases.get("r1");
         check(r1, TestType.RELEASE, InputKind.FILE, "r1.in",
-                OutputKind.REFERENCE_IMPL,  null, "python2.7 r1.py", "python2.7 r1-instructor.py");
+                ExpectedKind.REFERENCE_IMPL,  null, "python2.7 r1.py", "python2.7 r1-instructor.py");
         ExecutableTestCase r2 = cases.get("r2");
         check(r1, TestType.RELEASE, InputKind.STRING, "input for r2",
-                OutputKind.REFERENCE_IMPL,  null, "python2.7 r2.py", "python2.7 r2-instructor.py");
+                ExpectedKind.REFERENCE_IMPL,  null, "python2.7 r2.py", "python2.7 r2-instructor.py");
     }
 
   private   Map<String, ExecutableTestCase> getTestProperties(String testProps) throws IOException {
@@ -61,7 +61,7 @@ public class ExecutableTestCaseTest extends TestCase {
           TestType testType,
           InputKind inputKind, 
           String input,
-          OutputKind outputKind,
+          ExpectedKind outputKind,
           String output,
           String exec) {
       check(testCase, testType, inputKind, input, outputKind, output, exec, null);
@@ -72,7 +72,7 @@ public class ExecutableTestCaseTest extends TestCase {
           TestType testType,
           InputKind inputKind, 
           String input,
-          OutputKind outputKind,
+          ExpectedKind outputKind,
           String output, String execSource, String  referenceExecSource) {
       assertEquals(inputKind, testCase.getInputKind());
       if (inputKind.hasValue())
@@ -88,7 +88,7 @@ public class ExecutableTestCaseTest extends TestCase {
       if (!Arrays.equals(exec, actualExec))
           throw new AssertionError("Expected " + Arrays.toString(exec) 
                   +", actual " + Arrays.toString(actualExec));
-      if (outputKind == OutputKind.REFERENCE_IMPL) {
+      if (outputKind == ExpectedKind.REFERENCE_IMPL) {
           String [] referenceExec = referenceExecSource.split("\\s+");
           String[] actualReferenceExec = testCase.getStrings(ExecutableTestCase.Property.REFERENCE_EXEC);
           if (!Arrays.equals(referenceExec, actualReferenceExec))
